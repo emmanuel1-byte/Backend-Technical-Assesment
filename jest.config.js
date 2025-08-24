d809c9750257afdb972da3831c8508895229c26b
Emmanuel <emmanuelhilary9@gmail.com>
2025-08-24 08:24:45 +0100

    update: removed config props

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,6 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  testMatch: ["**/src/test/unit/**/*.spec.ts"], // only TS unit tests
+  testMatch: ["**/src/test/**/*.spec.ts"],
   moduleFileExtensions: ["ts", "js", "json", "node"],
-  clearMocks: true,
-  collectCoverage: true,
-  coverageDirectory: "coverage",
 };
